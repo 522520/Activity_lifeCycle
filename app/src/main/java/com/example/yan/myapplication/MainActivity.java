5f9e1e2dabb425af8e7e707ab9b6fd81beda6d70
Yan <1543045635@qq.com>
2018-03-24 23:36:00 +0800

    3.24

--- app/src/main/java/com/example/yan/myapplication/MainActivity.java
+++ app/src/main/java/com/example/yan/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.yan.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("1","onCreat:");
+
+    }
+    @Override
+    protected  void onStart(){
+        super.onStart();
+        Log.d("2","onStart:");
+    }
+    @Override
+    protected  void onResume(){
+        super.onResume();
+        Log.d("3","onResume:");
+    }
+    @Override
+    protected  void onPause(){
+        super.onPause();
+        Log.d("4","onPause:");
+    }
+    @Override
+    protected  void onStop(){
+        super.onStop();
+        Log.d("5","onStop:");
+    }
+    @Override
+    protected  void onDestroy(){
+        super.onDestroy();
+        Log.d("6","onDestroy:");
     }
 }
